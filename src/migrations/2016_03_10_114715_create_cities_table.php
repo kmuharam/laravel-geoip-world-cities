7c132b04213e4d955e4c6dc1bd719994cc6e9182
Khalid Moharrum <khalid.moharram@gmail.com>
2016-03-15 02:41:06 +0300

    Add migration file

---
+++ src/migrations/2016_03_10_114715_create_cities_table.php
@@ -0,0 +1,60 @@
+<?php
+
+use Moharrum\LaravelCities\Config;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCitiesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create(Config::citiesTableName(), function (Blueprint $table) {
+            $table->increments('id');
+            
+            $table->char('country', 2)
+                    ->nullable();
+            
+            $table->string('city')
+                    ->nullable();
+            
+            $table->string('city_ascii')
+                    ->nullable();
+            
+            $table->char('region', 2)
+                    ->nullable();
+
+            $table->integer('population')
+                    ->nullable()
+                    ->unsigned();
+
+            $table->decimal('latitude', 10, 6)
+                    ->nullable();
+
+            $table->decimal('longitude', 10, 6)
+                    ->nullable();
+            
+            $table->index('country', 'idx_country');
+            
+            $table->index('region', 'idx_region');
+            
+            $table->index(['latitude', 'longitude'], 'idx_lat_long');
+            
+            $table->timestamps = false;
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop(Config::citiesTableName());
+    }
+}
