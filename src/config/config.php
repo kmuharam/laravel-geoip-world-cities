1dd586ce7f3bf3483e40ef57f22508de47bb60b2
Khalid Moharrum <khalid.moharram@gmail.com>
2016-03-14 19:22:21 +0300

    Add config file

---
+++ src/config/config.php
@@ -0,0 +1,17 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Cities Defaults
+    |--------------------------------------------------------------------------
+    |
+    | The following options controls the default cities
+    | options for your application.
+    |
+    */
+
+    'table' => 'cities',
+
+];
