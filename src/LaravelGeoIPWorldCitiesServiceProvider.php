2c4c6a0b0eb0787ba995d3add2a32e2b0ae70139
Khalid Moharrum <khalid.moharram@gmail.com>
2016-03-14 19:08:33 +0300

    Init Service Provider configuration

--- src/LaravelGeoIPWorldCitiesServiceProvider.php
+++ src/LaravelGeoIPWorldCitiesServiceProvider.php
@@ -7,13 +7,24 @@ use Illuminate\Support\ServiceProvider;
 class LaravelGeoIPWorldCitiesServiceProvider extends ServiceProvider
 {
     /**
+     * Indicates if loading of the provider is deferred.
+     *
+     * @var bool
+     */
+    protected $defer = false;
+    
+    /**
      * Perform post-registration booting of services.
      *
      * @return void
      */
     public function boot()
     {
-        //
+        $this->publishes([
+            __DIR__.'/config/config.php' => config_path('cities.php'),
+            __DIR__.'/migrations/2016_03_10_114715_create_cities_table.php' => base_path('database/migrations/2016_03_10_114715_create_cities_table.php'),
+            __DIR__.'/seeds/CitiesTableSeeder.php' => base_path('database/seeds/CitiesTableSeeder.php'),
+        ]);
     }
 
     /**
@@ -23,6 +34,17 @@ class LaravelGeoIPWorldCitiesServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+        $this->app['cities'] = $this->app->share(function ($app) {
+                return new City();
+        });
+        $this->mergeConfig();
+    }
+
+    /**
+     * Merges user's and cities's configs.
+     */
+    private function mergeConfig()
+    {
+        $this->mergeConfigFrom(__DIR__.'/config/config.php', 'cities');
     }
 }
