faa7e6db6bdde799b4630c8e26265da1abf604e8
Khalid Moharrum <khalid.moharram@gmail.com>
2016-03-18 03:59:27 +0300

    Update the docs and handle() to be more user friendly

--- src/Console/CreateCitiesSeederCommand.php
+++ src/Console/CreateCitiesSeederCommand.php
@@ -1,10 +1,23 @@
 <?php
 
 namespace Moharrum\LaravelGeoIPWorldCities\Console;
+/*
+ * \Moharrum\LaravelGeoIPWorldCities for Laravel 5
+ *
+ * Copyright (c) 2015 - 2016 LaravelGeoIPWorldCities
+ *
+ * @copyright  Copyright (c) 2015 - 2016 \Moharrum\LaravelGeoIPWorldCities
+ * 
+ * @license http://opensource.org/licenses/MIT MIT license
+ */
 
 use Illuminate\Console\Command;
+use Illuminate\Support\Facades\File;
 use Moharrum\LaravelGeoIPWorldCities\Helpers\Config;
 
+/**
+ * @author Khalid Moharrum <khalid.moharram@gmail.com>
+ */
 class CreateCitiesSeederCommand extends Command
 {
     /**
@@ -45,62 +58,61 @@ class CreateCitiesSeederCommand extends Command
      */
     public function handle()
     {
-        $this->line('');
-        $this->info('We will create a seeder file for the cities table');
-        $this->line('');
+        if (File::exists(Config::publishedSeederRealpath())) {
+            if (!$this->confirm('The seeder file already exists, overwrite it? [Yes|no]')) {
+                $this->line('');
 
-        if($this->confirm("Proceed with the seeder creation? [Yes|no]")) {
-            $this->line('');
-
-            $this->info("Creating seeder file...");
-
-            // Laravel 5.0 does not have make:seed command
-            // in that case ignore the command and copy the contents
-            // of the file directly.
-            try {
-                $this->callSilent('make:seed', [
-                                                'name' => substr(
-                                                            Config::$SEEDER_FILE_NAME,
-                                                            0,
-                                                            strpos(
-                                                                Config::$SEEDER_FILE_NAME,
-                                                                '.'
-                                                            )
-                                                        )
-                                                ]
-                        );
-            } catch (\Exception $ex) {
-
+                return $this->info('Okay, no changes made to the file.');
             }
-
-            $inputFile = file_get_contents(Config::seederPath());
-
-            $outputFile = fopen(
-                            base_path(
-                                'database'
-                                .DIRECTORY_SEPARATOR
-                                .'seeds'
-                                .DIRECTORY_SEPARATOR
-                                .Config::$SEEDER_FILE_NAME
-                            ),
-                            'w'
-                        );
-
-            if($inputFile && $outputFile) {
-                fwrite($outputFile, $inputFile);
-                fclose($outputFile);
-            } else {
-                return $this->error(
-                            "Could not create the seeder file.\n Check write permissions "
-                            ."for database/seeds directory"
-                        );
-            }
-
-            $this->line('');
-
-            $this->info('Seeder created.');
-
-            $this->line('');
         }
+
+        // Laravel 5.0 does not have make:seed command
+        // in that case ignore the command and copy the contents
+        // of the file directly.
+        try {
+            $this->callSilent('make:seed', [
+                                            'name' => substr(
+                                                        Config::$SEEDER_FILE_NAME,
+                                                        0,
+                                                        strpos(
+                                                            Config::$SEEDER_FILE_NAME,
+                                                            '.'
+                                                        )
+                                                    )
+                                            ]
+                    );
+        } catch (\Exception $ex) {
+
+        }
+
+        $inputFile = file_get_contents(Config::localSeederPath());
+
+        $outputFile = fopen(
+                        Config::publishedSeederRealpath(),
+                        'w'
+                    );
+
+        if ($inputFile && $outputFile) {
+            fwrite($outputFile, $inputFile);
+
+            fclose($outputFile);
+        } else {
+            File::delete(Config::publishedSeederRealpath());
+
+            $this->line('');
+
+            return $this->error(
+                        'There was an error creating the seeder file, '
+                        .'check write permissions for database/seeds directory'
+                        .PHP_EOL
+                        .PHP_EOL
+                        .'If you think this is a bug, please submit a bug report '
+                        .'at https://github.com/moharrum/laravel-geoip-world-cities/issues'
+                    );
+        }
+
+        $this->line('');
+
+        $this->info('Okay, seeder file created successfully.');
     }
 }
